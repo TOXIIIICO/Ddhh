4f95497f7559565c095fed47112ffc9499566016
TOXIIIICO <156939831+TOXIIIICO@users.noreply.github.com>
2024-03-09 02:57:46 +0200

    Update OWNER.py

--- OWNER.py
+++ OWNER.py
@@ -1,7 +1,7 @@
 # متطلبات التنصيب #
 OWNER = ["ALH_KAR", "@M_O_lp"]
 OWNER_NAME = "👿☠️𝘼 𝙡 𝙈 𝙖 𝙧 𝙚 𝙗⸙ꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋ⧚آلَمِےـــــرعَےـــــبّ"
-BOT_TOKEN = "6434332622:AAF9hk8RKwyZhOXwx_ddMJafY7ed_55b3Jg"
+BOT_TOKEN = "6434332622:AAH-SSyJv9CB-PIwbQQBiRO_FfiUXVLOukU"
 DATABASE = "mongodb+srv://bot_vambir:Al2552001@cluster0.heabj.mongodb.net/vambir_bot?retryWrites=true&w=majority"
 CHANNEL = "https://t.me/HLV_M"
 GROUP = "https://t.me/ALTUNZAT"
